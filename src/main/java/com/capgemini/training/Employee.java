dcd285d2ea4881ae91d69b78f2523f4c52557b30
Utsab4704 <utsab.mahato20072003@gmail.com>
2026-03-14 15:19:07 +0530

    Initial commit

---
+++ src/main/java/com/capgemini/training/Employee.java
@@ -0,0 +1,25 @@
+package com.capgemini.training;
+
+public class Employee {
+	private String empName;
+	private String empId;
+	private String empEmail;
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getEmpEmail() {
+		return empEmail;
+	}
+	public void setEmpEmail(String empEmail) {
+		this.empEmail = empEmail;
+	}
+}
---
+++ src/main/java/com/capgemini/training/LoginController.java
@@ -0,0 +1,31 @@
+package com.capgemini.training;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping("/")
+	public String loginPage() {
+		return "login.jsp";
+	}
+	
+	@PostMapping("/login")
+	public String validate(String user, String password) {
+		System.out.println(user);
+		System.out.println(password);
+		return "home.jsp";
+	}
+	
+	@PostMapping("/register")
+	public String register(HttpServletRequest request){
+		String empId = request.getParameter("empId");
+		System.out.println("Emp Id : "+empId);
+		return "home.jsp";
+	}
+}
